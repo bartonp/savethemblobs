f01b6f9272258c19eed0130f2b972ba3d5778d20
Paul Barton <bartonp@users.users.github.com>
2016-09-02 13:17:21 +0100

    Cleaned up the savefromtu file

--- savefromtu.py
+++ savefromtu.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
-# savefromtu.py - save blobs etc from a device
+# savefromtu.py - save blobs etc from a device stored in tinyumbella's known device plist files
 
 import os
 import argparse
-import requests
 import savethemblobs
 import gzip
 import plistlib
 
-def get_user_friendly_name(identifier):
-	url = 'http://api.ios.icj.me/v2/%s/latest/name' % (identifier)
-	r = requests.get(url, headers={'User-Agent': savethemblobs.USER_AGENT})
-	return r.text
-
 def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--save-dir', help='local dir for saving blobs (default: ~/.shsh)', default=os.path.join(os.path.expanduser('~'), '.shsh'))
-	parser.add_argument('--overwrite', help='overwrite any existing blobs', action='store_true')
 	parser.add_argument('--overwrite-apple', help='overwrite any existing blobs (only from Apple)', action='store_true')
-	parser.add_argument('--overwrite-cydia', help='overwrite any existing blobs (only from Cydia)', action='store_true')
-	parser.add_argument('--overwrite-ifaith', help='overwrite any existing blobs (only from iFaith)', action='store_true')
-	parser.add_argument('--no-submit-cydia', help='don\'t submit blobs to Cydia server', action='store_true')
-	parser.add_argument('--skip-cydia', help='skip fetching blobs from Cydia server', action='store_true')
-	parser.add_argument('--skip-ifaith', help='skip fetching blobs from iFaith server', action='store_true')
 	parser.add_argument('--tu-path', help='Point to the Tinyumbrella config path (default ~/.tu)', default=os.path.join('~', '.tu'))
 	return parser.parse_args()
 
@@ -50,7 +38,6 @@ def get_tiny_umbrella_devices(tu_path):
 	for dirname, dirs, files in os.walk(tu_path):
 		for f in files:
 			devices.append(Device(os.path.join(dirname, f)))
-
 	return devices
 
 def main():
@@ -61,15 +48,13 @@ def main():
 		device.connect()
 
 		identifier = device.get_value(name=u'ProductType')
-
 		ecid = device.get_value(name=u'UniqueChipID')
-		print identifier, ecid
-		#print "Device: {1} [{1}]".format(device.get_value(name='DeviceName').encode('ascii'), identifier)
+
+		print "Device: {} [{}]".format(ecid, identifier)
 
 		args = parse_args()
 		args.device = identifier
 		args.ecid = str(ecid)
-		args.overwrite = False
 		args.no_submit_cydia = True
 		args.skip_cydia = True
 		args.skip_ifaith = True
